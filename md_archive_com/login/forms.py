a820dd171a93c2f314e49eaac37f3d2616acba46
OscarCampusNET <oscar.navarro.campusnet@gmail.com>
2022-12-02 18:37:12 +0100

    Update forms.py

--- md_archive_com/login/forms.py
+++ md_archive_com/login/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 class CreateNewList(forms.Form):
-    name = forms.CharField(label="ToDo List Name", max_length=200)
+    name = forms.CharField(label="Nombre", max_length=200)
     surname= forms.CharField(label="Apellidos", max_length=200)
     email = forms.CharField(label="Correo Electronico", max_length=100)
+    password = forms.CharField(label="Contraseña", min_length=8, max_length=100)
