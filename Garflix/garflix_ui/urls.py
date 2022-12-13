c3b8e2e5818d8475ce9ae6fa97f6faac1c7ad25a
OscarCampusNET <oscar.navarro.campusnet@gmail.com>
2022-12-13 18:42:16 +0100

    Update urls.py

--- Garflix/garflix_ui/urls.py
+++ Garflix/garflix_ui/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-]
+    path('<int:id>', views.index, name="index"),
+    path("home", views.home, name="home"),
+    path("create", views.create, name="create"),
+    path("register", views.register, name="register"),
+    path("view/", views.view, name="view"),
+    ]
