db697fe25a51dc4667febcbb667b2a0b534df16a
md-archive <dev@md-archive.com>
2022-12-13 15:55:44 +0000

    Delete md_archive_com directory

--- md_archive_com/login/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class CreateNewList(forms.Form):
-    name = forms.CharField(label="Nombre", max_length=200)
-    surname= forms.CharField(label="Apellidos", max_length=200)
-    email = forms.CharField(label="Correo Electronico", max_length=100)
-    password = forms.CharField(label="Contraseña", min_length=8, max_length=100)
