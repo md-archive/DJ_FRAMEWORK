03e1d3e7e903e8de9b67870aa2126019a5ce5091
root <root@dev.md-archive.com>
2022-12-13 17:34:33 +0000

    uplaod

--- Garflix/Garflix/settings.py
+++ Garflix/Garflix/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-3wx)#-pwrzx=7k^3$8hei#z177&)msz6iu=ox1x2r+4vk4bbx+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.garflix.GarflixConfig',
 ]
 
 MIDDLEWARE = [
