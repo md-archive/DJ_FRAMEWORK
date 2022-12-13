3a0e937c9d0982ba6c24691a6a2f6c6d0f75484a
root <root@dev.md-archive.com>
2022-12-13 18:29:17 +0000

    what_

--- Garflix/Garflix/settings.py
+++ Garflix/Garflix/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.garflix.GarflixConfig',
+    'garflix_ui.apps.GarflixUiConfig',
 ]
 
 MIDDLEWARE = [
--- Garflix/garflix_ui/views.py
+++ Garflix/garflix_ui/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
+def index(response):
+	return render(response, "garflix_ui/base.html", {})
