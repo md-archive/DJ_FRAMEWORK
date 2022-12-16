dd727a4a0ab6793db6bbc2bec09402a38324d05c
md-archive <dev@md-archive.com>
2022-12-16 17:20:23 +0100

    Download

--- md_archive_com/login/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class CreateNewList(forms.Form):
-    name = forms.CharField(label="ToDo List Name", max_length=200)
-    surname= forms.CharField(label="Apellidos", max_length=200)
-    email = forms.CharField(label="Correo Electronico", max_length=100)
