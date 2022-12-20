69a058add6fd374362c0b17636fb0cdaae17a05e
OscarCampusNET <oscar.navarro.campusnet@gmail.com>
2022-12-20 19:12:11 +0100

    Update urls.py

--- Garflix/garflix_ui/urls.py
+++ Garflix/garflix_ui/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('browse/', views.browse, name="browse"),
+    path('details/', views.details, name="details"),
+    path('profile/', views.profile, name="profile"),
+    path('streams/', views.streams, name="streams"),
 ]
